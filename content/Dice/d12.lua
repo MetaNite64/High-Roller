bf75a648d3d8c2872e50a360ab68dd8f72f92c27
MetaNite64 <nintendofanatic655@gmail.com>
2026-02-07 23:01:32 -0600

    (d12) fix rerolls

--- content/Dice/d12.lua
+++ content/Dice/d12.lua
@@ -18,13 +18,14 @@ HRLR_UTIL.Dice {
   end,
 
   use = function(self, card, area)
-    HRLR_UTIL.useDie(card)
+    local roll = HRLR_UTIL.useDie(card)
 
     G.E_MANAGER:add_event(Event({
       trigger = "after",
       delay = 1.3,
       func = function()
-        ease_dollars(card.ability.extra.value)
+        card.ability.extra.value = roll
+        ease_dollars(roll)
         SMODS.destroy_cards(card)
         return true
       end
